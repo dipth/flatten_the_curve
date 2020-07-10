a1ad4562b8b3c6338e5267cd6fdbdde5ed4972cb
CORSAIRONE\PS <pierreskinnerup@gmail.com>
2020-07-10 23:21:55 +0200

    Added UI Sprites and Fonts, Added tilemap.

---
+++ Assets/Scripts/Helpers/LogoScene.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LogoScene : MonoBehaviour
+{
+
+    public float countdownTimer = 4f;
+
+    private void Awake()
+    {
+        StartCoroutine(LoadNextScene());
+    }
+
+    IEnumerator LoadNextScene()
+    {
+        yield return new WaitForSeconds(countdownTimer);
+        SceneManager.LoadScene("Menu");
+    }
+}
---
+++ Assets/Scripts/Managers/MenuManager.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    public void StartGame() 
+    {
+    
+    }
+
+    public void OpenSettings() 
+    {
+    
+    }
+
+    public void CloseSettings() 
+    {
+    
+    }
+
+    public void OpenQuitPrompt() 
+    {
+    
+    }
+
+    public void CloseQuitPrompt() 
+    {
+    
+    }
+
+    public void CloseGame() 
+    {
+        Application.Quit();
+    }
+}
