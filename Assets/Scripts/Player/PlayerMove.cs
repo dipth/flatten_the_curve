21b336cd4f30a13268e5e9eb12fcd6ffcfeda777
CORSAIRONE\PS <pierreskinnerup@gmail.com>
2020-07-11 00:59:25 +0200

    Working on PlayerMove, added walking, sprinting, stamina system WIP...

--- Assets/Scripts/Player/PlayerMove.cs
+++ Assets/Scripts/Player/PlayerMove.cs
@@ -1,19 +1,101 @@
 ﻿
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
+    enum playerMoveState
+    {
+        walking,sprinting
+    }
+
+    private playerMoveState moveState;
+
+    [SerializeField]private float walkSpeed;
+    [SerializeField]private float sprintSpeed;
+
+    private Rigidbody2D rigidbody2D;
+    private float moveSpeed;
+    private float stamina = 100f;
+    private float staminaRegenTimer;
+    private float staminaRegenDelay = 2f;
+
+    private Vector2 moveDir;
+
+
+    private void Awake()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        moveSpeed = walkSpeed;
+        moveState = playerMoveState.walking;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        HandleInput();
+        HandleStamina();
+        SetPlayerSpeed();
+    }
+
+    private void SetPlayerSpeed()
+    {
+        if (moveState == playerMoveState.sprinting)
+            moveSpeed = sprintSpeed;
+        else
+            moveSpeed = walkSpeed;
+    }
+
+    private void HandleStamina()
+    {
+        if (moveState == playerMoveState.sprinting)
+        {
+            stamina -= .5f;
+        }
+        else if(moveState == playerMoveState.walking) 
+        {
+            if (stamina < 100 && staminaRegenTimer < Time.time)
+            {
+                stamina += .5f;
+            }
+        }
+    }
+
+    private void HandleInput() 
+    {
+        if (Input.GetAxisRaw("Sprint") > 0)
+        {
+            if (stamina > 0)
+            {
+                moveState = playerMoveState.sprinting;
+                staminaRegenTimer = Time.time + staminaRegenDelay;
+            }
+            else 
+            {
+                moveState = playerMoveState.walking;
+            }
+        }
+        else
+        {
+            moveState = playerMoveState.walking;
+        }
+
+
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+
+        moveDir = new Vector2(x * moveSpeed, y * moveSpeed);
+    }
+
+    private void FixedUpdate()
+    {
+        rigidbody2D.velocity = moveDir;
     }
 }
