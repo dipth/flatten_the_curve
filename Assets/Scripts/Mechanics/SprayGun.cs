4aaff59806bac3f638efd7249c7ba4583c73699d
dipth <thomas@dippel.dk>
2020-07-12 14:47:19 +0200

    started work on the spray gun

---
+++ Assets/Scripts/Mechanics/SprayGun.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SprayGun : MonoBehaviour
+{
+    [SerializeField] ParticleSystem particles;
+    [SerializeField] Collider2D collider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        collider.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FireGun()
+    {
+        collider.enabled = true;
+        particles.Play();
+    }
+
+    private void ScareNPCs()
+    {
+        // TODO: Implement me
+    }
+}
