acf294fd0545f43ec8c94363fdb23aa8b5164530
dipth <thomas@dippel.dk>
2020-07-12 18:28:07 +0200

    add total citizens left indicator

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Xml.Serialization;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -7,14 +8,17 @@ public class GameManager : MonoBehaviour
 {
     public static GameManager instance;
 
-    private int maxCitizen = 67;
-    [SerializeField] private Text citizenText;
+    private int maxCitizens = 67;
+    private int savedCitizens = 0;
+    [SerializeField] private Text totalCitizensLeftLabel;
     [SerializeField] private AudioSource musicSource;
 
     private void Awake()
     {
         if (instance == null)
             instance = this;
+
+        RefreshTotalCitizensLeftLabel();
     }
 
     private void Update()
@@ -30,7 +34,8 @@ public class GameManager : MonoBehaviour
     //Keep track of citi.
     public void AddCitizen() 
     {
-        
+        savedCitizens++;
+        RefreshTotalCitizensLeftLabel();
     }
 
     //Close app.
@@ -43,5 +48,10 @@ public class GameManager : MonoBehaviour
     void ToggleMusic() 
     {
         musicSource.enabled = !musicSource.enabled;
+    }
+
+    private void RefreshTotalCitizensLeftLabel()
+    {
+        totalCitizensLeftLabel.text = $"{savedCitizens}/{maxCitizens}";
     }
 }
